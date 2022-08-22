887b12a03cceae81b6fbf0c1269a71b1433aa83a
volkovask <volkov.ask@yandex.ru>
2022-08-23 00:56:43 +0700

    ver.2.0. add-friends-likes

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
